1be04ac88e5c3080c918fdb807f39af6529ba498
madhurachavan07 <chavanmadhura2222@gmail.com>
2026-09-19 11:21:15 +0530

    Day 6 Task Tracker Mini Project

---
+++ Day6/app.js
@@ -0,0 +1,64 @@
+const API_URL = "http://localhost:5000/tasks";
+
+function loadTasks() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(tasks => {
+            const taskList = document.getElementById("taskList");
+            taskList.innerHTML = "";
+
+            tasks.forEach(task => {
+                const li = document.createElement("li");
+
+                li.innerHTML = `
+                    ${task.title} - ${task.completed ? "Completed" : "Pending"}
+                    <button onclick="toggleTask(${task.id})">Complete</button>
+                    <button onclick="deleteTask(${task.id})">Delete</button>
+                `;
+
+                taskList.appendChild(li);
+            });
+        });
+}
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: input.value
+        })
+    })
+    .then(response => response.json())
+    .then(() => {
+        input.value = "";
+        loadTasks();
+    });
+}
+
+function toggleTask(id) {
+    fetch(`${API_URL}/${id}`, {
+        method: "PUT"
+    })
+    .then(response => response.json())
+    .then(() => loadTasks());
+}
+
+function deleteTask(id) {
+    fetch(`${API_URL}/${id}`, {
+        method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(() => loadTasks());
+}
+
+loadTasks();
